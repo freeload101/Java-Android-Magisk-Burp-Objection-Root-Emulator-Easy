17243b882af954f1692043b0a01c93862ea380ec
Robert McCurdy <freeload101@users.noreply.github.com>
2022-09-26 10:24:11 -0400

    Cleaned it up a bit

--- JAMBOREE.ps1
+++ JAMBOREE.ps1
@@ -4,12 +4,10 @@ TODO:
 * Clean/wipe install/cache 
 * actualy download latest cmdline tools
 
-
 PRECHECK/REQUIREMENTS:
 * Intel for HAXM
 * Hyperv disabled
 * Enabled Xesst in BIOS
-
 
 CLEANUP:
 rd q/s "c:\Python310\"
@@ -18,30 +16,25 @@ rd q/s "c:\Users\internet\AndroidStudioProjects"
 rd q/s "c:\Users\internet\AppData\Local\Android"
 rd q/s "c:\Users\internet\AppData\Local\Google"
 
+-RedirectStandardOutput RedirectStandardOutput.txt -RedirectStandardError RedirectStandardError.txt
 
 #>
-
-
-
-# REMOVE FOR IDE
-Set-Location -Path "C:\JAMBOREE"
-Stop-process -name java.exe -Force  -ErrorAction SilentlyContinue
-
-# env 
-    $env:ANDROID_SDK_ROOT = "$VARCD"
-    $env:ANDROID_AVD_HOME = "$VARCD"
-    $env:ANDROID_HOME = "$VARCD"
-    $env:ANDROID_AVD_HOME="$VARCD\avd"
-    New-Item -Path "$VARCD\avd" -ItemType Directory  -ErrorAction SilentlyContinue
-    $env:ANDROID_SDK_HOME="$VARCD"
-
-    $env:JAVA_HOME = "$VARCD\jdk-11.0.1"
-    $env:Path = "$env:Path;$VARCD\platform-tools\"
-
 
 # set current directory
 $VARCD = (Get-Location)
 Write-Host "[+] Current Working Directory $VARCD"
+Set-Location -Path "$VARCD"
+
+# env 
+$env:ANDROID_SDK_ROOT="$VARCD"
+$env:ANDROID_AVD_HOME="$VARCD"
+$env:ANDROID_HOME="$VARCD"
+$env:ANDROID_AVD_HOME="$VARCD\avd"
+New-Item -Path "$VARCD\avd" -ItemType Directory  -ErrorAction SilentlyContinue |Out-Null
+$env:ANDROID_SDK_HOME="$VARCD"
+
+$env:JAVA_HOME = "$VARCD\jdk-11.0.1"
+$env:Path = "$env:Path;$VARCD\platform-tools\"
 
 # Setup Form
 Add-Type -assembly System.Windows.Forms
@@ -55,14 +48,45 @@ $vShift = 0
 
 ### MAIN ###
 
+################################# FUNCTIONS
+
+
+############# downloadFile
+function downloadFile($url, $targetFile)
+{
+    "Downloading $url"
+    $uri = New-Object "System.Uri" "$url"
+    $request = [System.Net.HttpWebRequest]::Create($uri)
+    $request.set_Timeout(15000) #15 second timeout
+    $response = $request.GetResponse()
+    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+    $responseStream = $response.GetResponseStream()
+    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+    $buffer = new-object byte[] 10KB
+    $count = $responseStream.Read($buffer,0,$buffer.length)
+    $downloadedBytes = $count
+    while ($count -gt 0)
+    {
+        #[System.Console]::CursorLeft = 0
+        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength)
+        $targetStream.Write($buffer, 0, $count)
+        $count = $responseStream.Read($buffer,0,$buffer.length)
+        $downloadedBytes = $downloadedBytes + $count
+    }
+    "Finished Download"
+    $targetStream.Flush()
+    $targetStream.Close()
+    $targetStream.Dispose()
+    $responseStream.Dispose()
+}
+
 
 ############# CHECK JAVA
 Function CheckJava {
    if (-not(Test-Path -Path "$VARCD\jdk-11.0.1" )) { 
         try {
             Write-Host "[+] Downloading Java"
-            $downloadUri = "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
-            Invoke-WebRequest -Uri $downloadUri -Out "$VARCD\openjdk.zip"
+            downloadFile "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip" "$VARCD\openjdk.zip"
             Write-Host "[+] Extracting Java"
             Expand-Archive -Path  "$VARCD\openjdk.zip" -DestinationPath "$VARCD" -Force
             $env:JAVA_HOME = "$VARCD\jdk-11.0.1"
@@ -78,6 +102,21 @@ Function CheckJava {
 }
 
 
+
+
+################################# FUNCTIONS END
+function CheckADB {
+    $varadb = (adb devices)
+    Write-Host "[+] $varadb"
+    $varadb = $varadb -match 'device\b' -replace 'device',''
+    Write-Host "[+] Online Device is: $varadb"
+        if (($varadb.length -lt 1 )) {
+            Write-Host "[+] ADB Failed"
+            [void][System.Windows.MessageBox]::Show("ADB Failed. Wait and try again","Error")
+        }
+    return $varadb
+}
+
 ############# BUTTON1
 $Button1 = New-Object System.Windows.Forms.Button
 $Button1.AutoSize = $true
@@ -87,12 +126,8 @@ $Button1.Add_Click({Button1})
 $main_form.Controls.Add($Button1)
 
 Function Button1 {
-$varadb = (adb devices)
-Write-Host "[+] Current ADB devices:"
-Write-Host "[+] $varadb"
-$varadb = $varadb -match 'device\b' -replace 'device',''
-Write-Host "[+] Connecting to $varadb"
-Start-Process -FilePath "$VARCD\platform-tools\adb.exe" -ArgumentList  "-s $varadb shell " -Wait
+    $varadb=CheckADB
+    Start-Process -FilePath "$VARCD\platform-tools\adb.exe" -ArgumentList  "-s $varadb shell " -Wait  
 }
 
 ############# BUTTON2
@@ -107,8 +142,7 @@ Function Button2 {
     if (-not(Test-Path -Path "$VARCD\cmdline-tools" )) {
         try {
             Write-Host "[+] Downloading Android Command Line Tools"
-            $downloadUri = "https://dl.google.com/android/repository/commandlinetools-win-8512546_latest.zip"
-            Invoke-WebRequest -Uri $downloadUri -Out "$VARCD\commandlinetools-win.zip"
+            downloadFile "https://dl.google.com/android/repository/commandlinetools-win-8512546_latest.zip" "$VARCD\commandlinetools-win.zip"
             Write-Host "[+] Extracting AVD"
             Expand-Archive -Path  "$VARCD\commandlinetools-win.zip" -DestinationPath "$VARCD" -Force 
             Write-Host "[+] Setting path to latest that AVD wants ..."
@@ -155,7 +189,9 @@ $Button3.Add_Click({Button3})
 $main_form.Controls.Add($Button3)
 
 Function Button3 {
-Start-Process -FilePath "$VARCD\cmdline-tools\latest\bin\avdmanager.bat" -ArgumentList  "create avd -n pixel_2 -k `"system-images;android-30;google_apis_playstore;x86`"  -d `"pixel_2`" --force" -Wait   -Verbose -RedirectStandardOutput RedirectStandardOutput.txt -RedirectStandardError RedirectStandardError.txt
+    Start-Process -FilePath "$VARCD\cmdline-tools\latest\bin\avdmanager.bat" -ArgumentList  "create avd -n pixel_2 -k `"system-images;android-30;google_apis_playstore;x86`"  -d `"pixel_2`" --force" -Wait   -Verbose  -RedirectStandardError error.txt -RedirectStandardOutput output.txt
+    Get-Content "$VARCD\error.txt"
+    Get-Content "$VARCD\output.txt"
 }
 
 ############# BUTTON4
@@ -167,7 +203,8 @@ $Button4.Add_Click({Button4})
 $main_form.Controls.Add($Button4)
 
 Function Button4 {
-Start-Process -FilePath "$VARCD\emulator\emulator.exe" -ArgumentList  " -avd pixel_2 -writable-system"  
+    Write-Host "[+] Starting AVD emulator"
+    Start-Process -FilePath "$VARCD\emulator\emulator.exe" -ArgumentList  " -avd pixel_2 -writable-system"  -WindowStyle Minimized
 }
 
 ############# BUTTON5
@@ -179,45 +216,40 @@ $Button5.Add_Click({Button5})
 $main_form.Controls.Add($Button5)
 
 Function Button5 {
-$varadb = (adb devices)
-Write-Host "[+] Current ADB devices:"
-Write-Host "[+] $varadb"
-$varadb = $varadb -match 'device\b' -replace 'device',''
-Write-Host "[+] Connecting to $varadb"
-Start-Process -FilePath "$VARCD\platform-tools\adb.exe" -ArgumentList  "-s $varadb shell -t  `"reboot -p`"" -Wait
+    $varadb=CheckADB
+    Start-Process -FilePath "$VARCD\platform-tools\adb.exe" -ArgumentList  "-s $varadb shell -t  `"reboot -p`"" -Wait
 }
 
 ############# Button6
 $Button6 = New-Object System.Windows.Forms.Button
 $Button6.AutoSize = $true
-$Button6.Text = "rootAVD"
+$Button6.Text = "rootAVD/Install Magisk"
 $Button6.Location = New-Object System.Drawing.Point(($hShift),($vShift+150))
 $Button6.Add_Click({Button6})
 $main_form.Controls.Add($Button6)
 
 Function Button6 {
-Write-Host "[+] Downloading rootAVD"
-# C:\JAMBOREE\system-images\android-30\google_apis_playstore\x86\ramdisk.img
-# https://github.com/newbit1/rootAVD/archive/refs/heads/master.zip
-            cd  C:\JAMBOREE\rootAVD-master
-            $downloadUri = "https://github.com/newbit1/rootAVD/archive/refs/heads/master.zip"
-            Invoke-WebRequest -Uri $downloadUri -Out "$VARCD\rootAVD-master.zip"
+if (-not(Test-Path -Path "$VARCD\rootAVD-master" )) {
+    try {
+            Write-Host "[+] Downloading rootAVD"
+            downloadFile "https://github.com/newbit1/rootAVD/archive/refs/heads/master.zip" "$VARCD\rootAVD-master.zip"
             Write-Host "[+] Extracting rootAVD (Turn On AVD 1st" 
             Expand-Archive -Path  "$VARCD\rootAVD-master.zip" -DestinationPath "$VARCD" -Force
-            
-            Write-Host "[+] Getting CUrrent AVD "
-            adb kill-server
-            adb kill-server
-            adb kill-server
-            Start-Sleep -Seconds 5    
-            $varadb = (adb devices)
-            Write-Host "[+] Current ADB devices:"
-            Write-Host "[+] $varadb"
-            $varadb = $varadb -match 'device\b' -replace 'device',''
-            Write-Host "[+] Connecting to $varadb"
-
-            Write-Host "[+] Running installing magisk via rootAVD to ramdisk.img"
-            Start-Process -FilePath "$VARCD\rootAVD-master\rootAVD.bat" -ArgumentList  "$VARCD\system-images\android-30\google_apis_playstore\x86\ramdisk.img"    -RedirectStandardOutput RedirectStandardOutput.txt -RedirectStandardError RedirectStandardError.txt
+        }
+            catch {
+            throw $_.Exception.Message
+            }
+        }
+        else {
+            Write-Host "[+] $VARCD\rootAVD-master already exists"
+        }
+    
+    Set-Location -Path "$VARCD\rootAVD-master"
+    $varadb=CheckADB   
+    Write-Host "[+] Running installing magisk via rootAVD to ramdisk.img"
+    Start-Process -FilePath "$VARCD\rootAVD-master\rootAVD.bat" -ArgumentList  "$VARCD\system-images\android-30\google_apis_playstore\x86\ramdisk.img" -Wait 
+    Write-Host "[+] rootAVD Finished if the emulator did not close/poweroff try again"
+    
 }
  
 <#
@@ -235,7 +267,6 @@ cd  rootAVD-master
 rootAVD.bat "C:\JAMBOREE\system-images\android-30\google_apis_playstore\x86\ramdisk.img"
 #>
 
+
 ############# SHOW FORM
 $main_form.ShowDialog()
-
-
