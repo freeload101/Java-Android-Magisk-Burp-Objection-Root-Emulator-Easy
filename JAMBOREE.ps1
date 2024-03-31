6297c1e66a2b9778dd1a5f5e6ff2fb2cc8b79348
Robert McCurdy <freeload101@users.noreply.github.com>
2024-03-31 16:28:04 -0400

    Fixed Node download again lol

--- JAMBOREE.ps1
+++ JAMBOREE.ps1
@@ -318,8 +318,7 @@ Function CheckNode {
    if (-not(Test-Path -Path "$VARCD\node" )) {
         try {
 			Write-Message  -Message  "Downloading latest node" -Type "INFO"
-			$downloadUri = (Invoke-RestMethod -Method GET -Uri "https://nodejs.org/en/download/")    -split '>' -match '.*win-x64.*zip.*'  | ForEach-Object {$_ -ireplace '.* href="','' -ireplace  '".*',''}
-            downloadFile "$downloadUri" "$VARCD\node.zip"
+			$downloadUri = $downloadUri = (Invoke-RestMethod -Method GET -Uri "https://nodejs.org/dist/latest/")    -split '"'    -match '.*node-.*-win-x64.zip.*'   | ForEach-Object {$_ -ireplace '^','https://nodejs.org/dist/latest/' }  | select -first 1
 			Write-Message  -Message  "Extracting Node"  -Type "INFO"
 			Add-Type -AssemblyName System.IO.Compression.FileSystem
             Add-Type -AssemblyName System.IO.Compression
